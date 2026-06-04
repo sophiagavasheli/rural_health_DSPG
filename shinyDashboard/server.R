8d7c7a3a09fb749b85d46a25bce46db8acf5f5f5
corenhuff <corenhuff@vt.edu>
2026-06-04 14:44:51 -0400

    Added chart and played with shiny app

--- shinyDashboard/server.R
+++ shinyDashboard/server.R
@@ -4,17 +4,33 @@ library(shinythemes)
 library(shinyjs)
 library(tidyverse)
 
+chr <- read.csv("../cleaned_data/cleanCHR25.csv")
+
+
 #server
-server = function(input, output) {
-  output$greeting = renderText({"Hello world!"})
+server <- function(input, output) {
+  output$greeting = renderText({"Background on Health and Infrastructure in Rural Virginia"})
   
   output$normal_plot = renderPlot({
     samples = rnorm(1000, 0, 1) #random normal vector
     hist(samples, breaks=30, col="maroon", main="histogram", xlab="val")
   })
+
+  output$welcome<- renderText({
+    paste0("Welcome to our website, ", input$name, "!")
+    })
   
   
+  output$chr_plot <- renderPlot({
+    ggplot(chr, aes(Pct_Households_with_Broadband_Access, Years_of_Potential_Life_Lost_Rate)) +
+      geom_point(alpha = 0.5)+
+      xlab("% Households with Broadband Access")+
+      ylab("YPLL Rate per 100,000")
+    })
   
+  output$results <- renderText({
+    paste("Results will go here.")
+  })
   
   
 }
--- shinyDashboard/ui.R
+++ shinyDashboard/ui.R
@@ -15,18 +15,48 @@ ui <- navbarPage(
     "Overview",
     fluidPage(
       textOutput(outputId = "greeting"),
-      plotOutput(outputId = 'normal_plot')
+      plotOutput(outputId = 'normal_plot'),
+      textOutput(outputId = 'welcome'),
+      textInput(inputId = 'name', 
+                label = 'Enter your name',
+                value = 'Your name here'),
+      plotOutput(outputId = 'chr_plot')
+      
+      
     )
   ),
   
   tabPanel(
     "Results",
     fluidPage(
+      textOutput(outputId = 'results')
       # additional content
     )
+  ), 
+  
+  tabPanel(
+    "Data Sources", 
+    fluidRow(style = "margin-left: 50px; margin-right: 50px; font-size: 16px; font-family: 'Times New Roman', Times, serif;",
+             h1("Data Sources", align = "center")
+      #additional content
+    )
+  ), 
+  
+  tabPanel(
+    "About Us", 
+    fluidPage(
+      style = "margin-left: 50px; margin-right: 50px; font-size: 16px; font-family: 'Times New Roman', Times, serif;",
+      h1("Meet the Interns!", 
+         align = "center"), 
+      div(
+        style = "text-align:center;",
+        img(src = "interns.jpg", 
+          height = "400px", 
+          width = "700px")
+      #additional content
+    )
   )
-  
-  
+  )
   
   
   
