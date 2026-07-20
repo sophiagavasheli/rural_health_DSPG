bc5c0c51f2b12fe862bdaebb67bc82abeb28840e
sophiagavasheli <91988724+sophiagavasheli@users.noreply.github.com>
2026-07-20 10:41:39 -0400

    figures for poster

---
+++ figures/poster_RF_figures.R
@@ -0,0 +1,190 @@
+# figures for poster
+
+library(dplyr)
+library(ggplot2)
+library(purrr)
+
+outdir = "figures/poster_figs"
+
+pal <- c(
+  "#861F41",
+  "maroon",  
+  "#D96C1F", 
+  "goldenrod2"  
+)
+
+
+save_plot <- function(plot, filename, width = 10, height = 7){
+  ggsave(
+    filename = file.path(outdir, filename),
+    plot = plot,
+    width = width,
+    height = height,
+    dpi = 300
+  )
+}
+
+imp = readRDS("shiny_dashboard/many_year_grf_importance.rds")
+
+perf = readRDS("shiny_dashboard/many_year_grf_performance.rds")
+#pred = readRDS("shiny_dashboard/many_year_grf_predictions.rds")
+
+perf = perf %>% 
+  mutate(model_demo = paste(model, demographics, sep = "_")) %>% 
+  mutate(
+    model_demo = factor(
+      model_demo,
+      levels = c(
+        "full_model_test_yes",
+        "selected_model_test_yes",
+        "full_model_test_no",
+        "selected_model_test_no"
+      )
+    )
+  )
+
+
+p_rmse_mae <- perf %>% 
+  filter(statistic %in% c("RMSE","MAE")) %>%
+  ggplot(aes(x = statistic, y = value, fill = model_demo)) +
+  geom_col(position="dodge") +
+  facet_wrap(~health_outcome, scales = "free_y") +
+  scale_fill_manual(
+    name = "",
+    values = c(
+      full_model_test_yes = pal[1],
+      selected_model_test_yes = pal[2],
+      full_model_test_no = pal[3],
+      selected_model_test_no = pal[4]
+    ),
+    breaks = c("full_model_test_yes", "selected_model_test_yes", "full_model_test_no", "selected_model_test_no"),
+    labels = c("Full Model with Demographics", "Selected Model with Demographics", "Full Model without Demographics", "Selected Model without Demographics")
+  ) +
+  labs(
+    title="Mean Absolute Errors and Root Mean Squared Errors for Random Forest Models by Health Outcome",
+    x="Statistic",
+    y="Value") +
+  theme_bw() +
+  theme(
+    plot.title = element_text(hjust = 0.5),
+    legend.position = "bottom",
+    legend.direction = "horizontal",
+    legend.box = "horizontal"
+  )
+
+save_plot(p_rmse_mae, "rmse_mae.png")
+
+p_r2 <- perf %>% 
+  filter(statistic == "R2") %>%
+    ggplot(aes(x = statistic, y = value, fill = model_demo)) +
+    geom_col(position="dodge") +
+    facet_wrap(~health_outcome, scales = "free_y") +
+    scale_fill_manual(
+      name = "",
+      values = c(
+        full_model_test_yes = pal[1],
+        selected_model_test_yes = pal[2],
+        full_model_test_no = pal[3],
+        selected_model_test_no = pal[4]
+      ),
+      breaks = c("full_model_test_yes", "selected_model_test_yes", "full_model_test_no", "selected_model_test_no"),
+      labels = c("Full Model with Demographics", "Selected Model with Demographics", "Full Model without Demographics", "Selected Model without Demographics")
+    ) +
+    labs(
+      title="R2 Values for Random Forest Models by Health Outcome",
+      x="Statistic",
+      y="Value") +
+    theme_bw() +
+    theme(
+      plot.title = element_text(hjust = 0.5),
+      legend.position = "bottom",
+      legend.direction = "horizontal",
+      legend.box = "horizontal"
+    )
+
+save_plot(p_r2, "r2.png")
+
+plot_importance <- function(outcome_name, label){
+  
+  p_dem <- imp %>%
+    filter(outcome == outcome_name) %>%
+    filter(demographics == "yes") %>% 
+    arrange(desc(importance)) %>%
+    slice_max(importance, n = 10) %>%
+    ggplot(
+      aes(
+        x = reorder(variable, importance),
+        y = importance
+      )
+    ) +
+    geom_col(fill = pal[1]) +
+    coord_flip() +
+    scale_x_discrete(
+      labels = \(x) stringr::str_wrap(x, width = 30)
+    ) +
+    labs(
+      title = paste0("Variable Importance in Predicting ", label, ", Demographics Included"),
+      x = "Predictor Variable",
+      y = "Variable Importance"
+    ) +
+    theme_bw() +
+    theme(
+      plot.title = element_text(hjust = 0.5)
+    )
+  
+  
+  save_plot(
+    p_dem,
+    paste0(gsub(" ", "_", label), "_dem.png")
+  )
+  
+  p_no_dem <- imp %>%
+    filter(outcome == outcome_name) %>%
+    filter(demographics == "no") %>% 
+    arrange(desc(importance)) %>%
+    slice_max(importance, n = 10) %>%
+    ggplot(
+      aes(
+        x = reorder(variable, importance),
+        y = importance
+      )
+    ) +
+    geom_col(fill = pal[1]) +
+    coord_flip() +
+    scale_x_discrete(
+      labels = \(x) stringr::str_wrap(x, width = 30)
+    ) +
+    labs(
+      title = paste0("Variable Importance in Predicting ", label, ", Demographics Excluded"),
+      x = "Predictor Variable",
+      y = "Variable Importance"
+    ) +
+    theme_bw() +
+    theme(
+      plot.title = element_text(hjust = 0.5)
+    )
+  
+  
+  save_plot(
+    p_no_dem,
+    paste0(gsub(" ", "_", label), "_no_dem.png")
+  )
+  
+}
+
+health_labels <- tribble(
+  ~variable,                          ~label,
+  "CDCA_STROKE_DTH_RATE_ABOVE35",     "Stroke Mortality Rate (Age 35+)",
+  "CDCW_SELFHARM_DTH_RATE",           "Self-Harm Mortality Rate",
+  "CDCW_INJURY_DTH_RATE",             "Injury Mortality Rate",
+  "CHR_PCT_ADULT_OBESITY",            "Adult Obesity Prevalence",
+  "CHR_PCT_LOW_BIRTH_WT",             "Low Birth Weight Prevalence",
+  "CHR_PCT_MENTAL_DISTRESS",          "Frequent Mental Distress Prevalence"
+)
+pwalk(
+  list(
+    outcome_name = health_labels$variable,
+    label = health_labels$label
+  ),
+  plot_importance
+)
